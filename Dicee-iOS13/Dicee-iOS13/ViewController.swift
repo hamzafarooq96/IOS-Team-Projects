905add4363c9251b741409bb6fe7c387f8c60dcf
Hamza Farooq <51108114+hamzafarooq96@users.noreply.github.com>
2020-05-16 14:20:16 +0500

    Comitted Code

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //IBOutlet UI Elements
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+  
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+        
+        
+    }
+    
+}
+
---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by IOS Team on 15/05/2020.
+//  Copyright © 2020 IOS Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
